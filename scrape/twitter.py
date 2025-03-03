3787f1cfcc2e14f3612cbfe0ba8fad21938be036
vocal <48099292+coolest@users.noreply.github.com>
2025-03-03 09:36:16 -0600

    Start twitter scraping

--- scrape/twitter.py
+++ scrape/twitter.py
@@ -1,9 +1,43 @@
-# find twitter scrappers and see if there is any
+import tweepy
+import os
+import argparse
+import asyncio
 
-# get authenticate for bot etc
+import config
 
-# do same cli stuff like reddit
+# Authentication
+consumer_key = os.getenv("X_API_KEY")
+consumer_secret = os.getenv("X_API_KEY_SECRET")
+access_token = os.getenv("X_ACCESS_TOKEN")
+access_token_secret = os.getenv("X_ACCESS_TOKEN_SECRET")
+    
+# Set up auth
+auth = tweepy.OAuth1UserHandler(
+   consumer_key, consumer_secret,
+   access_token, access_token_secret
+)
 
-# scrape etc
+# API object
+api = tweepy.API(auth)
 
-# same db stuff
+async def handle_twitter(hashtag, start, end):
+    # Search here, look up db, etc
+    # May have to limit to get ~50 per day (?) or ~25 per day (?)
+    
+    return
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hashtag")
+    parser.add_argument("--start")
+    parser.add_argument("--end")
+    
+    args = parser.parse_args()
+    
+    if (not args.start or not args.end or not args.hashtag):
+        return
+    
+    await handle_twitter(args.hashtag, int(args.start), int(args.end))
+
+if __name__ == "__main__":
+    asyncio.run(main())
